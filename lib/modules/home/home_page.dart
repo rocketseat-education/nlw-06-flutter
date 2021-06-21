5f366216120688a2643ade5762d62c936a144117
Gabriel Savio <gabrielsavio@Gabriels-Mac-Pro.local>
2021-06-21 20:44:22 -0300

    aula 2

---
+++ lib/modules/home/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("HomePage"),
+      ),
+    );
+  }
+}
---
+++ lib/modules/splash/splash_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_images.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Stack(
+        children: [
+          Center(child: Image.asset(AppImages.union)),
+          Center(child: Image.asset(AppImages.logoFull)),
+        ],
+      ),
+    );
+  }
+}
