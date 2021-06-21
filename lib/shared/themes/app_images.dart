e34e6bc9e2efb7c19dd0fe68a06d40e76ad2b8d3
Gabriel Savio <gabrielsavio@Gabriels-Mac-Pro.local>
2021-06-21 12:46:15 -0300

    add images no projeto

---
+++ lib/shared/themes/app_images.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppImages {
+  static const logoFull = "assets/images/logofull.png";
+  static const logomini = "assets/images/logomini.png";
+  static const union = "assets/images/union.png";
+  static const person = "assets/images/person.png";
+  static const google = "assets/images/google.png";
+}
